c75b2911773e841c587c805090b335d58fdb007d
Barakonda <anirozeemail@gmail.com>
2026-01-30 01:42:53 +0200

    Set Attack Buttons

--- Assets/Scripts/Player/AttackButton.cs
+++ Assets/Scripts/Player/AttackButton.cs
@@ -5,22 +5,38 @@ public class AttackButton : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Button _button;
-    [SerializeField] private PlayerCombat player; 
-    [SerializeField] private int attackIndex; 
+    [SerializeField] private PlayerCombat player;
+    [SerializeField] private int attackIndex;
+
     void Start()
     {
         _button = GetComponentInParent<Button>();
         _button.clicked += () =>
         {
-            //player.SetActiveMasque(masqueType);
+            switch (attackIndex)
+            {
+                case 0:
+                    player.AttackBasic();
+                    break;
+                case 1:
+                    player.AttackMedium();
+                    break;
+                case 2:
+                    player.AttackStrong();
+                    break;
+                case 3:
+                    //player.Defend();
+                    break;
+            }
         };
     }
 
-    // Update is called once per frame
+// Update is called once per frame
     void Update()
     {
-        // var isActive = player.GetActiveMasque()?.type == masqueType;
-        // var isBroken = player.GetActiveMasque()?.durability > 0;
-        // _button.SetEnabled(isActive);
+        if (attackIndex == 3)
+            _button.text = "Defend";
+        else
+            _button.text = player.GetActiveMasque().attacks[attackIndex].name;
     }
 }
