c85a96b1ee1e84e4f41047ee756867f055c65252
Beru <lidor.y.shapira@gmail.com>
2026-01-30 13:36:53 +0200

    added a bunch of mexican planets data

---
+++ Assets/Scripts/Enums/PlanetState.cs
@@ -0,0 +1,6 @@
+public enum PlanetState
+{
+    Open,
+    Locked,
+    Defeated
+}
---
+++ Assets/Scripts/SO/Planets/PlanetData.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlanetData", menuName = "Scriptable Objects/PlanetData")]
+public class PlanetData : ScriptableObject
+{
+    public string planetName;
+    [CanBeNull] public string previousPlanet;
+    public PlanetState state;
+    public Sprite currentSprite;
+    public Sprite baseSprite;
+    public Sprite lockedSprite;
+    public Sprite defeatedSprite;
+
+    public void ChangeState(PlanetState newState)
+    {
+        state = newState;
+        currentSprite = newState switch
+        {
+            PlanetState.Locked => lockedSprite,
+            PlanetState.Defeated => defeatedSprite,
+            _ => baseSprite
+        };
+    }
+}
