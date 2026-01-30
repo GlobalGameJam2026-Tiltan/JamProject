985ee8bf76c1d36f0c63563704e9863f890ca39c
Beru <lidor.y.shapira@gmail.com>
2026-01-30 14:46:10 +0200

    some more fixes

--- Assets/Scripts/SO/EnemyData.cs
+++ Assets/Scripts/SO/EnemyData.cs
@@ -8,13 +8,5 @@ public class EnemyData : ScriptableObject
     public Sprite sprite;
     public Sprite icon;
     public AttackOption[] attacks = new AttackOption[3];
-    public AudioSource audioSource;
     public Sprite[][] AttackAnimations = new Sprite[3][];
-
-    public void Attack()
-    {
-        var rnd = Random.Range(0, 3);
-        audioSource.PlayOneShot(attacks[rnd].attackVoiceLine);
-        // TODO: play animation AttackAnimations[rnd]
-    }
 }
