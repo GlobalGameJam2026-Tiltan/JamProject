977fbbe50094784f4371adadacf6bc2273c94f04
Beru <lidor.y.shapira@gmail.com>
2026-01-30 16:02:44 +0200

    fixed boss again

--- Assets/Scripts/SO/Enemies/Boss/BossData.cs
+++ Assets/Scripts/SO/Enemies/Boss/BossData.cs
@@ -6,7 +6,9 @@ public class BossData : ScriptableObject
     public float[] healths = new float[3];
     public float[] maxHealths = new float[3];
     public Sprite sprite;
-    public Sprite icon;
+    public Sprite strIcon;
+    public Sprite intIcon;
+    public Sprite chaIcon;
     public AttackOption[] phase1Attacks = new AttackOption[9];
     public AttackOption[] phase2Attacks = new AttackOption[9];
     public AttackOption[] phase3Attacks = new AttackOption[9];
