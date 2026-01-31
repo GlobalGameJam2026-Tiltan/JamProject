81bfa1fa6869e6f3d13761c248f1661c0c1b446b
Beru <lidor.y.shapira@gmail.com>
2026-01-31 14:18:04 +0200

    added a shit-ton of animations

--- Assets/Scripts/Enemies/EnemyAnimator.cs
+++ Assets/Scripts/Enemies/EnemyAnimator.cs
@@ -3,28 +3,70 @@ using UnityEngine;
 public class EnemyAnimator : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer bodyRenderer;
-    [SerializeField] private SpriteRenderer maskRenderer;
+    [SerializeField] private SpriteRenderer masqueRenderer;
 
     private BodyAnimationSet _bodySet;
     private MasqueAnimationSet _masqueSet;
+    
+    private Sprite[] _bossPunch;
+    private Sprite[] _bossGrab;
+    
+    private bool _hasMasque;
+    private bool _isBoss;
 
-    public void Initialize(
+    // ---------- ENEMY ----------
+    public void InitializeEnemy(
         EnemyData data,
         EnemyAnimationLibrary library)
     {
         _bodySet = library.GetBody(data.bodyType);
-        _masqueSet = library.GetMask(data.type);
+
+        _hasMasque = masqueRenderer != null;
+
+        if (_hasMasque)
+        {
+            _masqueSet = library.GetMask(data.type);
+        }
+    }
+    
+    // ---------- BOSS ----------
+    public void InitializeBoss(BossData data)
+    {
+        _isBoss = true;
+        _hasMasque = false;
+
+        _bossPunch = data.punchAnimation;
+        _bossGrab = data.grabAnimation;
     }
 
+    // ---------- PLAY ----------
     public void PlayGrab(int frame)
     {
-        bodyRenderer.sprite = _bodySet.grabFrames[frame];
-        maskRenderer.sprite = _masqueSet.grabFrames[frame];
+        if (_isBoss)
+        {
+            bodyRenderer.sprite = _bossGrab[frame];
+        }
+        else
+        {
+            bodyRenderer.sprite = _bodySet.grabFrames[frame];
+
+            if (_hasMasque)
+                masqueRenderer.sprite = _masqueSet.grabFrames[frame];
+        }
     }
 
     public void PlayPunch(int frame)
     {
-        bodyRenderer.sprite = _bodySet.punchFrames[frame];
-        maskRenderer.sprite = _masqueSet.punchFrames[frame];
+        if (_isBoss)
+        {
+            bodyRenderer.sprite = _bossPunch[frame];
+        }
+        else
+        {
+            bodyRenderer.sprite = _bodySet.punchFrames[frame];
+
+            if (_hasMasque)
+                masqueRenderer.sprite = _masqueSet.punchFrames[frame];
+        }
     }
 }
